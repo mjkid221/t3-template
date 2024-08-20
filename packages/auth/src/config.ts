b46e774022596398da4b6460a8968498be9c8294
MJ Lee <jamerytstudio@gmail.com>
2024-08-21 09:05:28 +1000

    Initial commit

---
+++ packages/auth/src/config.ts
@@ -0,0 +1,56 @@
+import type {
+  DefaultSession,
+  NextAuthConfig,
+  Session as NextAuthSession,
+} from "next-auth";
+import { skipCSRFCheck } from "@auth/core";
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
+import Discord from "next-auth/providers/discord";
+
+import clientPromise from "@acme/db/db";
+
+import { env } from "../env";
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id: string;
+    } & DefaultSession["user"];
+  }
+}
+
+const adapter = MongoDBAdapter(clientPromise);
+
+export const isSecureContext = env.NODE_ENV !== "development";
+
+export const authConfig = {
+  adapter,
+  // In development, we need to skip checks to allow Expo to work
+  ...(!isSecureContext
+    ? {
+        skipCSRFCheck: skipCSRFCheck,
+        trustHost: true,
+      }
+    : {}),
+  secret: env.AUTH_SECRET,
+  providers: [Discord],
+} satisfies NextAuthConfig;
+
+export const validateToken = async (
+  token: string,
+): Promise<NextAuthSession | null> => {
+  const sessionToken = token.slice("Bearer ".length);
+  const session = await adapter.getSessionAndUser?.(sessionToken);
+  return session
+    ? {
+        user: {
+          ...session.user,
+        },
+        expires: session.session.expires.toISOString(),
+      }
+    : null;
+};
+
+export const invalidateSessionToken = async (token: string) => {
+  await adapter.deleteSession?.(token);
+};
